143b682b8e85687741f038ae8eba53b2b63258b2
Jan Ciger <jan.ciger@reviatech.com>
2013-08-23 17:41:19 +0200

    Don't redefine MS types
    These types are not used in Wiiuse and conflict with MSVC 2012, because
    that defines int_fast16_t as int (among other things).

--- src/wiiuse_msvcstdint.h
+++ src/wiiuse_msvcstdint.h
@@ -89,27 +89,6 @@ extern "C" {
 typedef signed __int64       int64_t;
 typedef unsigned __int64     uint64_t;
 
-
-// 7.18.1.2 Minimum-width integer types
-typedef int8_t    int_least8_t;
-typedef int16_t   int_least16_t;
-typedef int32_t   int_least32_t;
-typedef int64_t   int_least64_t;
-typedef uint8_t   uint_least8_t;
-typedef uint16_t  uint_least16_t;
-typedef uint32_t  uint_least32_t;
-typedef uint64_t  uint_least64_t;
-
-// 7.18.1.3 Fastest minimum-width integer types
-typedef int8_t    int_fast8_t;
-typedef int16_t   int_fast16_t;
-typedef int32_t   int_fast32_t;
-typedef int64_t   int_fast64_t;
-typedef uint8_t   uint_fast8_t;
-typedef uint16_t  uint_fast16_t;
-typedef uint32_t  uint_fast32_t;
-typedef uint64_t  uint_fast64_t;
-
 // 7.18.1.4 Integer types capable of holding object pointers
 #ifdef _WIN64 // [
    typedef signed __int64    intptr_t;
