b1bebab834e0d64173cf5d55c1f9b7b50cc4a91e
Ryan Pavlik <ryan@sensics.com>
2016-04-25 14:43:11 -0500

    Fix macro redefinitions when compiling with VS 2015

--- src/definitions_os.h
+++ src/definitions_os.h
@@ -42,8 +42,13 @@
 #ifdef _MSC_VER
 	#include <float.h>
 	/* windows with visual c */
+	#if _MSC_VER >= 1900
+	/* 2015 includes the following in math.h now */
+	#include <math.h>
+	#else
 	#define isnan(x)		(_isnan(x))
 	#define isinf(x)		(!_finite(x))
+	#endif
 	/* disable warnings I don't care about */
 	/*#pragma warning(disable:4273)	*/	/* inconsistent dll linkage			*/
 #endif
